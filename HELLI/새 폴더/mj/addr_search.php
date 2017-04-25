5bfdf6a6fc300c1787e64afe4071d55d3f946b86
Pharaoh\Juyoung <zxc998x2@gmail.com>
2017-04-25 20:43:22 +0900

    HELLI

---
+++ HELLI/새 폴더/mj/addr_search.php
@@ -0,0 +1,33 @@
+<?php
+//mysqli_fetch_fields
+require("config/config.php");
+require("lib/db.php");
+$conn=db_init($config["host"],$config["duser"],$config["dpw"],$config["dname"]);
+
+mysqli_set_charset($conn,"utf8");
+
+$selected_sigun = $_POST['Selected'];
+$type = $_POST['Type'];
+
+if($type == "sigun"){
+	$sql = "SELECT DISTINCT SUBSTRING_INDEX(name, ' ', 2)
+    		FROM ac
+    		WHERE name like '$selected_sigun%'
+			ORDER BY name";	
+}
+
+if($type == "dong"){
+	$sql = "SELECT  DISTINCT SUBSTRING_INDEX(name, ' ', 3)
+    		FROM ac
+    		WHERE name like '$selected_sigun%'
+    		ORDER BY name";	
+}
+
+//
+
+$result = mysqli_query($conn,$sql);
+$row = mysqli_fetch_all($result);
+
+echo json_encode($row);
+
+?>
